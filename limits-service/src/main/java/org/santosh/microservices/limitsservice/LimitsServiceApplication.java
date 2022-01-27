1e466fba2d190b7baa52ed0b34d873739acff428
Santoshkumar Engandhula <santosh.kumar02@rediffmail.com>
2022-01-27 16:28:05 +0530

    Upgrade to Java 11

--- limits-service/src/main/java/org/santosh/microservices/limitsservice/LimitsServiceApplication.java
+++ limits-service/src/main/java/org/santosh/microservices/limitsservice/LimitsServiceApplication.java
@@ -2,7 +2,6 @@ package org.santosh.microservices.limitsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
